eaf1aa109c620be091496df98cae4f414abaf83e
Aldo Gatica <aldogatica123@gmail.com>
2019-03-15 21:59:54 -0600

    Fresh react app Material UI installed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Woop woop!
           </p>
           <a
             className="App-link"

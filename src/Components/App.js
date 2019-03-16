15a2d73d8daa994297547987d6d72ce0f274b96e
Aldo Gatica <aldogatica123@gmail.com>
2019-03-16 00:38:39 -0600

    SignIn page added App updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../css/App.css';
+import '../styles/App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import SignIn from './SignIn.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CssBaseline/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Woop woop!
+            Sistema eDent
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <SignIn/>
       </div>
     );
   }
---
+++ src/Components/SignIn.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import styles from '../styles/SignInStyle'
+import withStyles from "@material-ui/core/es/styles/withStyles";
+import '../styles/SignInStyle';
+import logo from '../img/edent-logo.png'
+
+function SignIn(props) {
+  const {classes} = props;
+  return (
+    <main className={classes.main}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar} src={logo} />
+        <Typography component="h1" variant="h5">
+          Iniciar Sesión
+        </Typography>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Correo electrónico</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus/>
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Contraseña</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="current-password"/>
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary"/>}
+            label="Recuérdame"/>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Iniciar Sesión
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
+}
+
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignIn);

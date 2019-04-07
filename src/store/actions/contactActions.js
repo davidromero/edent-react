074e37f8cad3576f9d1c2b7afadd62408856d321
Aldo Gatica <aldogatica123@gmail.com>
2019-04-06 23:26:25 -0600

    formatting issues fixed

--- src/store/actions/contactActions.js
+++ src/store/actions/contactActions.js
@@ -1,6 +1,6 @@
 export const createContact = (contact) => {
   return (dispatch, getState) => {
     //make async call
-    dispatch({ type: 'CREATE_CONTACT', contact})
+    dispatch({type: "CREATE_CONTACT", contact})
   }
 };

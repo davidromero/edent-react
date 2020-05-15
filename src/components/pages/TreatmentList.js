d3a340b7ef1d354f8f657d0145e243577ab202a2
Aldo Gatica <aldogatica123@gmail.com>
2020-05-14 23:06:09 -0600

    Treatment checkout updated Treatment cancel button added

--- src/components/pages/TreatmentList.js
+++ src/components/pages/TreatmentList.js
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from "react";
 import {Paper} from "@material-ui/core";
 import axios from 'axios/index';
 import "../styles/PagesStyle.css";
-import {Prompt} from "react-router-dom";
+import {Prompt, useHistory} from "react-router-dom";
+import Modal from "react-modal";
 
 const operatoriaMenu = [
   {treatment_uid: "123", treatment_name: "Resina"},
@@ -40,6 +41,7 @@ const TreatmentList = (props) => {
   const [checkout, setCheckout] = useState([]);
   const [treatmentType, setTreatmentType] = useState();
   const [patient, setPatient] = useState();
+  const history = useHistory();
 
   useEffect( () => {initValues()})
 
@@ -90,11 +92,15 @@ const TreatmentList = (props) => {
         when={true}
         message='Al salir el tratamiento se cancelará. ¿Está seguro?'
       />
-      <Paper className={"wide-paper"} elevation={2} square={false}>
-        {patient ?
-          <h2 style={{textTransform: "capitalize"}}>Nuevo Tratamiento: {patient.first_name + " " + patient.last_name}</h2> :
-          <h2>Nuevo Tratamiento</h2>}
-        <h3 style={{textTransform: "capitalize"}}>{treatmentType}</h3>
+      <Paper className={"wide-paper"} style={{display: "flex", justifyContent: "space-between", flexWrap: "wrap"}}
+             elevation={2} square={false}>
+        <div>
+          {patient ?
+            <h2 style={{textTransform: "capitalize"}}>Nuevo Tratamiento: {patient.first_name + " " + patient.last_name}</h2> :
+            <h2>Nuevo Tratamiento</h2>}
+          <h3 style={{textTransform: "capitalize"}}>{treatmentType}</h3>
+        </div>
+        <button className={"finish-treatment-button"} onClick={() => {history.goBack()}}>Cancelar Tratamiento</button>
       </Paper>
       <div style={{display: "table-column", width: "100%", justifyContent: "center"}}>
         <TreatmentCheckout checkout={checkout} remove={removeTreatment}/>
@@ -118,15 +124,54 @@ const TreatmentList = (props) => {
 
 const TreatmentCheckout = (props) => {
   const {checkout, remove} = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const finishTreatment = () => {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content : {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      margin: 'auto',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
+
+  const checkoutTotal =
+    <>
+      <h3><b>Total: Q300.00</b></h3>
+      <button className={"finish-treatment-button"} onClick={() => {setIsOpen(true)} }>Finalizar tratamiento</button>
+    </>
 
   return (
-    <Paper className={"lateral-paper"} elevation={2}>
-      <h3><b>Tratamientos en Progreso:</b></h3>
-      {
-        checkout.map((treatment, idx) =>
-          <TreatmentItem key={idx} idx={idx} treatment={treatment} remove={remove}/>)
-      }
-    </Paper>
+    <>
+      <Modal
+        isOpen={isOpen}
+        style={customStyles}
+        ariaHideApp={false}
+        contentLabel="¿Estas seguro?">
+        <h3>¿Está seguro en terminar el tratamiento?</h3>
+        <div className={"modal-container"}>
+          <button className="modal-button" style={{backgroundColor: "rgb(21, 149, 189)"}}
+                  onClick={finishTreatment}>Aceptar</button>
+          <button className="modal-button" style={{backgroundColor: "rgb(227,83,83)"}}
+                  onClick={() => {setIsOpen(false)}}>Cancelar</button>
+        </div>
+      </Modal>
+
+      <Paper className={"lateral-paper"} elevation={2}>
+        <h3><b>Tratamientos en Progreso:</b></h3>
+        {
+          checkout.map((treatment, idx) =>
+            <TreatmentItem key={idx} idx={idx} treatment={treatment} remove={remove}/>)
+        }
+        {checkout.length > 0 ? checkoutTotal : <></>}
+      </Paper>
+    </>
   )
 }
 
@@ -134,9 +179,9 @@ const TreatmentItem = (props) => {
   const {idx, treatment, remove} = props;
 
   return(
-    <div className={"participant-background"} key={idx}>
-      {treatment.name}
-      <button style={{width: "20%"}} className={"participant-button"} onClick={() => {remove(idx)}}>-</button>
+    <div className={"treatment-item"} key={idx}>
+      {treatment.name + " - Q" + "100"}
+      <button className={"remove-treatment-button"} onClick={() => {remove(idx)}}>x</button>
     </div>
   )
 }

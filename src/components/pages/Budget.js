0d92df554864a96f476d89c3e135f886173039e2
davidromero <d-romero@live.com>
2020-07-30 10:12:08 +0000

    ED-176, ED-179, Budget Creation and Export to pdf, need to refactor

--- src/components/pages/Budget.js
+++ src/components/pages/Budget.js
@@ -10,7 +10,7 @@ const Budget = () =>{
         <div>
           <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-between"}}>
             <div>
-              <h2>Creacion de Presupuesto</h2>
+              <h2>Creacion de Presupuesto </h2>
             </div>
           </div>
           <ServiceDetailBudget serviceName={"Operatoria"} treatmentId={"operatoria"}/>
